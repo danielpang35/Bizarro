e9abea657a8c8cce7abc4dd4976215fcf5004083
danielpang35 <53539894+danielpang35@users.noreply.github.com>
2022-09-26 22:01:08 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "bizarroStack.h"
 
 int main(void)
 {
-    
+    StackNodePtr top = NULL;
+    printf("Enter an integer: (CTRL Z TO EXIT)");
+    unsigned int input = 0;
+    scanf("%d", &input);
+    BizarreNumber_t num = (BizarreNumber_t) {.type = "SQ\0", .nrOfOpr = 0, .num = input};
+
+    push(&top, num);
+    printStack(top);
+    for(; input != EOF; scanf("%d", &input))
+    {
+        
+    }
+    //scanf("")
+
+
+    for(int i = 0; i < 5; i++)
+    {
+    }
 }
